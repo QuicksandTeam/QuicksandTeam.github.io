215af2fc7be68923de8d1249f81747cea25a06a6
aiyc <1432803776@qq.com>
2021-10-19 10:41:16 +0800

    update

--- aiyc.sh
+++ aiyc.sh
@@ -3,7 +3,9 @@ git status
 git add .
 git commit . -m "update"
 #git status
-git push -u origin
-git push -u quicksandio
+# git push -u origin
+# git push -u quicksandio
 # git push -u forkio
+git push -u origin main
+git push -u quicksandio main
 
