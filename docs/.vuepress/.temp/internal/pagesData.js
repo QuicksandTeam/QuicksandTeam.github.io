14614c7f4ac95c5e374295c99fe928a501fb5b60
aiyc <1432803776@qq.com>
2021-10-19 17:47:48 +0800

    update

--- docs/.vuepress/.temp/internal/pagesData.js
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -9,12 +9,14 @@ export const pagesData = {
   "v-4cd57d8f": () => import(/* webpackChunkName: "v-4cd57d8f" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/abouteam/about.html.js").then(({ data }) => data),
   // path: /zh/Children's_programming/
   "v-04a0befc": () => import(/* webpackChunkName: "v-04a0befc" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/Children's_programming/index.html.js").then(({ data }) => data),
-  // path: /zh/column/
-  "v-7c53b51e": () => import(/* webpackChunkName: "v-7c53b51e" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/column/index.html.js").then(({ data }) => data),
   // path: /zh/Programming_teaching/
   "v-47dece54": () => import(/* webpackChunkName: "v-47dece54" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/Programming_teaching/index.html.js").then(({ data }) => data),
+  // path: /zh/column/
+  "v-7c53b51e": () => import(/* webpackChunkName: "v-7c53b51e" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/column/index.html.js").then(({ data }) => data),
   // path: /zh/Technology_of_gossip/
   "v-315a5403": () => import(/* webpackChunkName: "v-315a5403" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/Technology_of_gossip/index.html.js").then(({ data }) => data),
+  // path: /zh/Pay_for_courses/pythonbase/
+  "v-6525aaf6": () => import(/* webpackChunkName: "v-6525aaf6" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/Pay_for_courses/pythonbase/index.html.js").then(({ data }) => data),
   // path: /zh/column/aiyuechuangsf/
   "v-0fb7d202": () => import(/* webpackChunkName: "v-0fb7d202" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/column/aiyuechuangsf/index.html.js").then(({ data }) => data),
   // path: /zh/column/Go/
@@ -23,8 +25,6 @@ export const pagesData = {
   "v-4817b4b2": () => import(/* webpackChunkName: "v-4817b4b2" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/column/jiuyuan_algorithm/01.html.js").then(({ data }) => data),
   // path: /zh/column/jiuyuan_algorithm/
   "v-74fb34b1": () => import(/* webpackChunkName: "v-74fb34b1" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/column/jiuyuan_algorithm/index.html.js").then(({ data }) => data),
-  // path: /zh/Pay_for_courses/pythonbase/
-  "v-6525aaf6": () => import(/* webpackChunkName: "v-6525aaf6" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/zh/Pay_for_courses/pythonbase/index.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"D:/gitee_all/bornforthi_book/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
 }
