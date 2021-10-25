71777931bc27b555d546c202c0aeb3ece51d63fc
aiyc <1432803776@qq.com>
2021-10-25 19:34:49 +0800

    update

--- aiyc.sh
+++ aiyc.sh
@@ -5,5 +5,5 @@ git commit . -m "update"
 # git branch -M main
 # git remote add origin git@github.com:forkcodeaiyc/forkcodeaiyc.github.io.git
 git push -u origin main
-git push -u origin1 main
+# git push -u origin1 main
 
