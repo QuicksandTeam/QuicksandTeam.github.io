9cb8890ef9257de8d9bce997741c15dbbd4f9e22
AndersonHJB <cleland1432803776@icloud.com>
2022-07-26 14:30:31 +0000

    Deploying to gh-pages from @ QuicksandTeam/QuicksandTeam.github.io@bc19a91a0986630fdf808a3822fd1c826507b96e 🚀

---
+++ assets/note.html.78449761.js
@@ -0,0 +1,31 @@
+import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";import{o as s,c as a,a as e}from"./app.6a44c9f4.js";const t={},i=e(`<div class="language-python ext-py line-numbers-mode"><pre class="language-python"><code>projects<span class="token punctuation">:</span>
+  <span class="token operator">-</span> icon<span class="token punctuation">:</span> project
+    name<span class="token punctuation">:</span> \u9879\u76EE\u540D\u79F0
+    desc<span class="token punctuation">:</span> \u9879\u76EE\u8BE6\u7EC6\u63CF\u8FF0
+    link<span class="token punctuation">:</span> https<span class="token punctuation">:</span><span class="token operator">//</span>\u4F60\u7684\u9879\u76EE\u94FE\u63A5
+
+  <span class="token operator">-</span> icon<span class="token punctuation">:</span> link
+    name<span class="token punctuation">:</span> \u94FE\u63A5\u540D\u79F0
+    desc<span class="token punctuation">:</span> \u94FE\u63A5\u8BE6\u7EC6\u63CF\u8FF0
+    link<span class="token punctuation">:</span> https<span class="token punctuation">:</span><span class="token operator">//</span>\u94FE\u63A5\u5730\u5740
+
+  <span class="token operator">-</span> icon<span class="token punctuation">:</span> book
+    name<span class="token punctuation">:</span> \u4E66\u7C4D\u540D\u79F0
+    desc<span class="token punctuation">:</span> \u4E66\u7C4D\u8BE6\u7EC6\u63CF\u8FF0
+    link<span class="token punctuation">:</span> https<span class="token punctuation">:</span><span class="token operator">//</span>\u4F60\u7684\u4E66\u7C4D\u94FE\u63A5
+
+  <span class="token operator">-</span> icon<span class="token punctuation">:</span> article
+    name<span class="token punctuation">:</span> \u6587\u7AE0\u540D\u79F0
+    desc<span class="token punctuation">:</span> \u6587\u7AE0\u8BE6\u7EC6\u63CF\u8FF0
+    link<span class="token punctuation">:</span> https<span class="token punctuation">:</span><span class="token operator">//</span>\u4F60\u7684\u6587\u7AE0\u94FE\u63A5
+
+  <span class="token operator">-</span> icon<span class="token punctuation">:</span> friend
+    name<span class="token punctuation">:</span> \u4F19\u4F34\u540D\u79F0
+    desc<span class="token punctuation">:</span> \u4F19\u4F34\u8BE6\u7EC6\u4ECB\u7ECD
+    link<span class="token punctuation">:</span> https<span class="token punctuation">:</span><span class="token operator">//</span>\u4F60\u7684\u4F19\u4F34\u94FE\u63A5
+
+  <span class="token operator">-</span> icon<span class="token punctuation">:</span> <span class="token operator">/</span>logo<span class="token punctuation">.</span>svg
+    name<span class="token punctuation">:</span> \u81EA\u5B9A\u4E49\u9879\u76EE
+    desc<span class="token punctuation">:</span> \u81EA\u5B9A\u4E49\u8BE6\u7EC6\u4ECB\u7ECD
+    link<span class="token punctuation">:</span> https<span class="token punctuation">:</span><span class="token operator">//</span>\u4F60\u7684\u81EA\u5B9A\u4E49\u94FE\u63A5
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),p=[i];function c(o,l){return s(),a("div",null,p)}var r=n(t,[["render",c],["__file","note.html.vue"]]);export{r as default};
